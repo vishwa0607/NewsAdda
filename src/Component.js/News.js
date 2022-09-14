242b0066c2e1f6846ef614bdd0f4622602db97d5
VIshwajeet <jeet06071997@gmail.com>
2022-09-15 01:07:27 +0530

    http

--- src/Component.js/News.js
+++ src/Component.js/News.js
@@ -31,7 +31,7 @@ export default class News extends Component {
   }
 
   async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let url = `http://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -71,7 +71,7 @@ export default class News extends Component {
   // }
   fetchMoreData = async () => {
     this.setState({ page: this.state.page + 1 });
-    const url = `https://newsapi.org/v2/top-headlines?country=${
+    const url = `http://newsapi.org/v2/top-headlines?country=${
       this.props.country
     }&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${
       this.state.page + 1
